1e7d718c7fad62aa1791725b1b78c8a2b5810d77
Bruno Brito <bhdebrito@gmail.com>
2019-06-10 00:56:03 -0300

    example app

---
+++ DockerConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using FluentScheduler;
+
+namespace DockerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JobManager.JobException += JobManager_JobException;
+
+            var registry = new Registry();
+
+            // Run every 5 minutes
+            registry.Schedule<MyFiveMinutesTask>().ToRunNow().AndEvery(5).Minutes();
+
+            // It will run every day at 2 am
+            registry.Schedule<MyDailyTask>().ToRunEvery(1).Days().At(2, 0);
+
+            JobManager.Initialize(registry);
+
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void JobManager_JobException(JobExceptionInfo obj)
+        {
+            // Log Errors 
+        }
+    }
+
+    public class MyFiveMinutesTask : IJob
+    {
+        public void Execute()
+        {
+            // Do something usefull
+        }
+    }
+
+
+    public class MyDailyTask : IJob
+    {
+        public void Execute()
+        {
+            // Do something usefull
+        }
+    }
+}
